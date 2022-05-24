e4ff2680ad64d2d32daae4dd5c34f13c1e097a4b
Pavithra.M <Pavithra.M@EXIND3010-63.allfon.in>
2022-05-24 13:06:17 +0530

    spring jdbc

--- SpringJDBC/src/main/java/com/example/demo/ifaces/CrudRepository.java
+++ SpringJDBC/src/main/java/com/example/demo/ifaces/CrudRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import com.example.demo.entity.Product;
 
-public interface CrudRepository<T,R>{
+public interface CrudRepository<T>{
 	public int add(Product T);
 	public List<T> findAll();
 	public T findById(int id);
